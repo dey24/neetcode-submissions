a505062e143d7691ead50b806f3a24d9734bdd7c
Chiranjit Dey <chiranjitdey2000@gmail.com>
2026-04-28 19:50:44 +0530

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} word1
+     * @param {string} word2
+     * @return {string}
+     */
+    mergeAlternately(word1, word2) {
+        let resultStr  = '';
+
+        let greaterLength = (word1.length < word2.length ? word2.length : word1.length);
+        console.log(greaterLength);
+
+        for(let i=0; i<greaterLength; i++){
+            if(word1[i] >= 'a' || word1[i] <='z'){
+                resultStr = resultStr.concat(word1[i]);
+            }
+            if(word2[i] >= 'a' || word2[i] <= 'z'){
+                resultStr = resultStr.concat(word2[i]);
+            }
+        }
+
+        // console.log(resultStr)
+        return resultStr;
+    }
+}
