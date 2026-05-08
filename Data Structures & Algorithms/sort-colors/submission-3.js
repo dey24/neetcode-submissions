d654153c37c480587fbf37eea02b51d04b676900
Chiranjit Dey <chiranjitdey2000@gmail.com>
2026-05-08 11:31:33 +0530

    Add: sort-colors - submission-3

---
+++ Data Structures & Algorithms/sort-colors/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    sortColors(nums) {
+        // Use the three pointer approach here. ileft will handle 0s, mid will handled 1s, right will handle 2s.
+        let left = 0, mid = 0, right = nums.length -1;
+
+        while(mid<=right){
+            if(nums[mid] == 0){
+                [nums[left], nums[mid]] = [nums[mid], nums[left]];
+                mid++;
+                left++;
+            }
+            else if(nums[mid] == 1) mid++;
+            else{
+                [nums[mid], nums[right]] = [nums[right], nums[mid]];
+                right--;
+            }
+        }
+        return nums
+    }
+}   
