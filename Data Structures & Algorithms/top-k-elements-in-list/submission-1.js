df45c84b2334b1e4d8037066f96d1d984cbea50b
Chiranjit Dey <chiranjitdey2000@gmail.com>
2026-09-01 11:12:28 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let freqTable = {};
+
+        for(let i =0; i<nums.length; i++){
+            if(freqTable[nums[i]]){
+                freqTable[nums[i]] += 1
+            }
+            else{
+                freqTable[nums[i]] = 1;
+            }
+        }
+
+        freqTable = Object.entries(freqTable).sort((a,b) => b[1] - a[1]);
+
+        // console.log(freqTable);
+
+        let result = []
+        for(let i =0; i<k; i++){
+            result.push(freqTable[i][0]);
+        }
+        
+
+        // console.log(result)
+        return result
+    }
+}
