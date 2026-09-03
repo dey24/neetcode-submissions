8b6e6fed6e325508f7a306593bff0fde4163e1db
Chiranjit Dey <chiranjitdey2000@gmail.com>
2026-09-03 16:58:12 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        //thought of greedy but to build logic needed help
+        let profit = 0;
+
+        for(let i =1; i<prices.length; i++){
+            if(prices[i] > prices[i-1]){
+                profit += (prices[i] - prices[i-1]);
+            }
+        }
+
+        return profit
+    }
+}
