1b369834a959185491ef6b666fdd210bde3c38a1
Chiranjit Dey <chiranjitdey2000@gmail.com>
2026-09-04 18:00:21 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    firstMissingPositive(nums) {
+        let missing = 1;
+        let sortedNums = nums.sort((a,b) => a - b);
+
+        for(let i =0; i<sortedNums.length; i++){
+            if(nums[i] == missing){
+                missing++;
+            }
+        }
+        return missing;
+    }
+}
