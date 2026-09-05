df825b9f90929145046065a3520faa987b979a5c
Chiranjit Dey <chiranjitdey2000@gmail.com>
2026-09-05 20:11:00 +0530

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    subarraySum(nums, k) {
+        let count = 0;
+
+        for(let i=0; i<nums.length; i++){
+            if(nums[i] == k){
+                count++;
+            }
+            let j = i+1;
+            let sum = nums[i]; // -1, 0
+            while(j < nums.length){
+                sum += nums[j];
+
+                if(sum == k){
+                    count++;
+                }
+                j++
+            }
+        }
+        return count;
+    }
+}
