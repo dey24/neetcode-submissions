603c44247a7ae9298b785f03be5c7c7ac54bc306
Chiranjit Dey <chiranjitdey2000@gmail.com>
2026-09-13 10:44:05 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let map = new Map();
+
+        let left = 0;
+        let maxLen = 0;
+        for(let right = 0; right<s.length; right++){
+            if(map.has(s[right])){
+                left = Math.max(left, map.get(s[right]) + 1);
+            }
+
+            map.set(s[right], right);
+            maxLen = Math.max(maxLen, right-left+1);
+        }
+        return maxLen
+    }
+}
