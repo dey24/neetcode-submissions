7fb6ed11bacf2cbd3dffb90217902030ac4c226b
Chiranjit Dey <chiranjitdey2000@gmail.com>
2026-09-22 11:15:36 +0530

    Add: boats-to-save-people - submission-4

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-4.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} people
+     * @param {number} limit
+     * @return {number}
+     */
+    numRescueBoats(people, limit) {
+        let count = 0;
+        
+        people.sort((a,b) => a-b)
+        let left = 0;
+        let right = people.length - 1;
+        
+        while(left<=right){
+            if(people[left] + people[right] <= limit){
+                left++;
+            }
+            right--;
+            count++
+        }
+
+        return count;
+    }
+}
